e9bb5625a1cfa133c1f000dbc0ae59bc2604cb47
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-06 17:50:02 +0200

    Adds test for 3825 and specials

--- test/Nome-vgs-Lunde.js
+++ test/Nome-vgs-Lunde.js
@@ -12,4 +12,9 @@ var options = {
 options.postnummer = 3960
 tap.equal(isFara(options), true, 'Nome vgs, avdeling Lunde returns true from 3960 Stathelle')
 
+options.postnummer = 3803
+options.gatenavn = 'Rokkemakervegen'
+options.husnummer = '81'
+tap.equal(isFara(options), true, 'Nome vgs, avdeling Lunde returns true from 3803')
+
 // Hovedregel for 3801, 3803, 3810 og 3830 er SANN FARA - Samme på Specials
